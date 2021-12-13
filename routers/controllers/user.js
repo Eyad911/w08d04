63c3b866125dfad973d3bf67c7db6878bb54ac82
Eyad911 <Eyad.Abdulhamid.elq.js@tuwaiq.edu.sa>
2021-12-13 12:07:22 +0300

    update

--- routers/controllers/user.js
+++ routers/controllers/user.js
@@ -1,9 +1,20 @@
 const userModel = require("./../../db/models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 // const { options } = require("../routes/role");
 const SALT = Number(process.env.SALT);
 require("dotenv").config();
+
+
+
+const transport = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 const Register = async (req, res) => {
   const { email, password, userName, avatar, role } = req.body;
@@ -11,28 +22,53 @@ const Register = async (req, res) => {
   console.log(req.token);
   console.log(req);
   const hashPass = await bcrypt.hash(password, SALT);
+  let activeCode = "";
+  const characters = "0123456789";
+  for (let i = 0; i < 4; i++) {
+    activeCode += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+
   const newUser = new userModel({
     email: lowerEmail,
     password: hashPass,
+    passwordCode: "",
     userName,
     avatar,
+    activeCode,
     role,
   });
   newUser
     .save()
     .then((result) => {
-      res.status(201).json(result);
-    })
+      transport
+          .sendMail({
+            from: process.env.EMAIL,
+            to: lowerEmail,
+            subject: "Please confirm your account",
+            html: `<h1>Email Confirmation</h1>
+              <h2>Hello ${lowerEmail}</h2>
+              <h4>CODE: ${activeCode}</h4>
+              <p>Thank you for registering. Please confirm your email by entring the code on the following link</p>
+              <a href=https://social-media-project-frontend.herokuapp.com/verify_account/${result._id}> Click here</a>
+              </div>`,
+          })
+          .catch((err) => console.log(err));
+        res.status(201).json(result);
+      })
     .catch((err) => {
       res.status(400).json(err);
     });
 };
 
+
+
 const verifyAccount = async (req, res) => {
   const { id, code } = req.body;
 
   const user = await userModel.findOne({ _id: id });
-
+console.log(user);
   if (user.activeCode == code) {
     userModel
       .findByIdAndUpdate(id, { active: true, activeCode: "" }, { new: true })
@@ -75,8 +111,9 @@ const login = (req, res) => {
           const token = await jwt.sign(payload, secret, option);
           console.log("thistoken",token);
           if (hashedpass) {
+            if (result.active == true){
             res.status(200).json({ result, token });
-          } else {
+          }else{res.status(404).json("Active your Account");}} else {
             res.status(404).json("worng email or password");
           }
         } else {
